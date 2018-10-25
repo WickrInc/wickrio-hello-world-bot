e62a1ad64fe4773feee07b510ba7594f889e2a2d
Aaron <amishiyev@wickr.com>
2018-10-25 15:35:50 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# WickrIO Hello World Bot
+
+Wickr's Hello World Bot
+
+## Description:
+Responds to users messages with canned responses sent out in a numerical order
+* Note: This is a good template to edit and create a custom integration for Wickr
---
+++ hello_world_bot.js
@@ -0,0 +1,151 @@
+var addon = require('wickrio_addon');
+var fs = require('fs');
+
+process.title = "helloWorldBot";
+module.exports = addon;
+process.stdin.resume(); //so the program will not close instantly
+
+function exitHandler(options, err) {
+  if (err) {
+    console.log('Error:', err.stack);
+    addon.cmdStopAsyncRecvMessages();
+    console.log(addon.closeClient());
+    process.exit();
+  }
+  if (options.exit) {
+    addon.cmdStopAsyncRecvMessages();
+    console.log(addon.closeClient());
+    process.exit();
+  } else if (options.pid) {
+    addon.cmdStopAsyncRecvMessages();
+    console.log(addon.closeClient());
+    process.kill(process.pid);
+  }
+}
+
+//catches ctrl+c and stop.sh events
+process.on('SIGINT', exitHandler.bind(null, {
+  exit: true
+}));
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null, {
+  pid: true
+}));
+process.on('SIGUSR2', exitHandler.bind(null, {
+  pid: true
+}));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {
+  exit: true
+}));
+
+return new Promise(async (resolve, reject) => {
+  if (process.argv[2] === undefined) {
+    var client = await fs.readFileSync('client_bot_username.txt', 'utf-8');
+    client = client.trim();
+    var response = await addon.clientInit(client);
+    resolve(response);
+  } else {
+    var response = await addon.clientInit(process.argv[2]);
+    resolve(response);
+  }
+
+}).then(result => {
+  console.log(result);
+
+  var responseMessageList = [
+    "Hey there! Thanks for messaging me! I have a few helpful but random tips I can share in response to your messages, " +
+    "so please bear with me☺ If you have more questions than I have answers, head to Settings > Support in Wickr Pro. " +
+    "Way to go to protect your privacy!",
+
+    "JavaScript API:\n\n" +
+    "Have an idea for a cool integration for wickr? you can now implement it using our brand new Node.js API " +
+    "It is also possible to code integrations in different languages using our WickrIO REST API. For more information visit https://github.com/WickrInc\n\n",
+
+    "Here is how to set expiration on your messages:\n\n" +
+    "Expiration is the max time your message will live. Burn-On-Read (BOR) is how long your message will live once the " +
+    "recipient(s) has seen it. You can change both by tapping on the (i) next to any conversation name, at the top of " +
+    "your screen.",
+
+    "Fun fact\n" +
+    "Wickr is used in over a 150 countries, and has over 600,000 registered users. " +
+    "Congrats on being one of them, enjoy your security and privacy.",
+
+    "Did you know that Wickr Pro has a Hubot integration?\n\n" +
+    "When you add a bot, just select the hubot integration option from the menu." +
+    "With Hubot you get access to cool features such as Giphy and pug bombs and exisiting Wickr Integrations such as Slack, Uber and Salesforce Chatter." +
+    "You can also create your own custom integrations with Hubot or choose from hundreds of existing integrations and scripts available online on npmjs.com.\n\n",
+
+    "Sending files on Wickr Pro:\n\n" +
+    "You can now send photos, videos, and other ephemeral files up to 5GB via End-to-End encryption. Secure Screenshare & File Upload This feature supports collaboration " +
+    "and maximum data hygiene for you and the contacts you TRUST. If you do not trust the person you’re talking to, do " +
+    "not open files coming from them or send them photos/files you do not want to be saved. Stay safe!",
+
+    "Verification\n\n" +
+    "You’ll notice an orange dot around your contacts’ avatars – that means you have not yet verified them.\n\n" +
+    "You don’t have to, but in case you want to make sure you are talking to the right person, send them a key video " +
+    "verification request to establish trust between your Wickr Pro accounts.\n\n" +
+    "Check out our blog on this: https://medium.com/cryptoblog/key-verification-in-secure-messaging-bd93a1bf3d40",
+
+    "Passwords\n\n" +
+    "Important to know: there is no password reset on Wickr Pro – we don't know who you are which prevents us from " +
+    "verifying you to reset your password.\n\n" +
+    "So please remember your password☺",
+
+    "Privacy\n\n" +
+    "We built Wickr Pro to provide private communications to everyone.\n" +
+    "We take your privacy & security very seriously, learn more: www.wickr.com/security.\n\n" +
+    "Source code https://github.com/WickrInc/wickr-crypto-c. FAQ www.wickr.com/faq"
+  ];
+
+  addon.cmdStartAsyncRecvMessages(listen);
+  var wickrUsers = [];
+
+  function listen(message) {
+    var parsedData = JSON.parse(message);
+    var vGroupID = parsedData.vgroupid;
+    var location = find(vGroupID);
+    if (location === -1) {
+      wickrUsers.push({
+        vGroupID: vGroupID,
+        index: 0
+      });
+    }
+    var current = getIndex(vGroupID);
+    if (current > 9) {
+      location = find(vGroupID);
+      wickrUsers[location].index = 0;
+    }
+    current = getIndex(vGroupID);
+    if (current <= 9 && current != -1) {
+      try {
+        var csrm = addon.cmdSendRoomMessage(vGroupID, responseMessageList[current], '100', '60');
+      } catch (err) {
+        console.log(err);
+      }
+      location = find(vGroupID);
+      wickrUsers[location].index = current + 1;
+    }
+  }
+
+
+  function find(vGroupID) {
+    for (var i = 0; i < wickrUsers.length; i++) {
+      if (wickrUsers[i].vGroupID[0].localeCompare(vGroupID[0]) === 0)
+        return i;
+    }
+    return -1;
+  }
+
+  function getIndex(vGroupID) {
+    for (var i = 0; i < wickrUsers.length; i++) {
+      if (wickrUsers[i].vGroupID[0] === vGroupID[0]) {
+        return wickrUsers[i].index;
+      }
+    }
+  }
+}).catch(error => {
+  console.log(error);
+});
